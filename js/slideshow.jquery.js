9b1aec2b36438c5e25aa14fdc83b543b432d1484
foreline <dima@foreline.ru>
2013-09-24 14:52:41 +0400

    Bug fixes

--- js/slideshow.jquery.js
+++ js/slideshow.jquery.js
@@ -1,37 +1,48 @@
-
 	
 	var slideShow = {
-		animationTime: 800,
+		animationTime: 700,
 		slideShowActive: false,
+		slidesPause: 5000
+	};
+
+	slideShow.start = function(){
+		
+		slideShow.slideShowActive = true;
+
+		var t = setTimeout('slideShow.slideShow()', slideShow.slidesPause);
 	};
 	
 	slideShow.slideShow = function(){
 		
 		if ( slideShow.slideShowActive ) {
-			nextSlide();
+			slideShow.nextSlide();
 		}
 		
-		var t = setTimeout('slideShow()', 5000);
+		var t = setTimeout('slideShow.slideShow()', slideShow.slidesPause);
 	};
-	
+
 	slideShow.nextSlide = function(){
+		
+		slideWidth = parseInt($(".slide").first().css('width').replace('px', ''));
+		
 		$(".slides").animate({
-			left: "-=560"
+			left: "-=" + slideWidth
 		}, slideShow.animationTime, function(){
 			$(".slide").first().insertAfter($(".slide").last());
-			$(".slides").css('margin-left', '+=560px');
+			$(".slides").css('margin-left', '+=' + slideWidth + 'px');
 		});
 	};
-	
+
 	slideShow.prevSlide = function(){
 		$(".slide").last().insertBefore($(".slide").first());
 		$(".slides").css('margin-left', '-=560px');
-			
+
 		$(".slides").animate({
 			left: "+=560"
 		}, slideShow.animationTime);
 	};
-	
+
 	slideShow.stop = function(){
 		slideShow.slideShowActive = false;
 	};
+	
